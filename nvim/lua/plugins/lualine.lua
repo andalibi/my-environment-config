b19fb397d3963d8782ce21e32aa9d56f45e6e134
Ali Andalibi <ali.andalibi@outlook.com>
2022-12-18 11:41:34 +0330

    refactor(nvim): show full path in status line

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,7 +1,9 @@
 require("lualine").setup({
     options = {
         icons_enabled = true,
-        theme = "Tomorrow",
+        -- theme = "NeoSolarized",
+        -- theme = "Tomorrow",
+        theme = "everforest",
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
         disabled_filetypes = {},
@@ -10,7 +12,13 @@ require("lualine").setup({
     sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", { "diagnostics", sources = { "nvim_lsp" } } },
-        lualine_c = { "filename" },
+        lualine_c = {
+            {
+                "filename",
+                file_status = true,
+                path = 1,
+            },
+        },
         lualine_x = { "encoding", "fileformat", "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
