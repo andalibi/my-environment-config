d80d8be6c26d5ef548b0af8bd4a851813b835023
Ali Andalibi <ali.andalibi@outlook.com>
2022-12-18 11:46:51 +0330

    feat(nvim): add lsp_signature plugin

---
+++ nvim/lua/plugins/lsp_signature.lua
@@ -0,0 +1,9 @@
+--------------------------------------------------------------------------------
+--
+require("lsp_signature").setup({
+    log_path = vim.fn.expand("$HOME") .. "/tmp/sig.log",
+    debug = true,
+    hint_enable = false,
+    handler_opts = { border = "single" },
+    max_width = 80,
+})
